059d4f09681d20b239859c9f58b5eca3f26b111e
Libing <12110141@mail.sustech.edu.cn>
2022-11-23 13:45:16 +0800

    3

--- main.c
+++ main.c
@@ -5,25 +5,30 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include <cblas.h>
+#include <unistd.h>
+
 //  g++ main.c source.c -mavx2 -DWITH_AVX2 && ./a.out
 //sudo su -
 //dd if=/dev/zero of=/swapfile bs=1048576 count=30720
 //mkswap /swapfile1
 //swapon /swapfile1
 
+// cc -o mul main.c source.c -mavx2 -DWITH_AVX2 -I /usr/include/ -L/usr/lib -lopenblas -lpthread -lgfortran &&./mul
+//cmake . -DCMAKE_BUILD_TYPE=Release -Bbuild
 int main() {
 
-    clock_t start0, end0, start1, end1,  start2, end2,start3, end3,start4, end4;
+    clock_t start0, end0, start1, end1,  start2, end2,start3, end3,start4, end4,start5, end5;
 
-    Matrix m1;
-    int size = 16;
-    int sizelarge = 16;
-   createRamMatrix(&m1, size, size, 2);
-   Matrix m2;
-   createRamMatrix(&m2, size, size, 2);
-   Matrix m3;
-   Matrix m4;
-   Matrix m5;
+   //  Matrix m1;
+    int size = 8000;
+    // int sizelarge = 8;
+   // createRamMatrix(&m1, size, size, 2);
+   // Matrix m2;
+   // createRamMatrix(&m2, size, size, 2);
+   // Matrix m3;
+   // Matrix m4;
+   // Matrix m5;
 //
     MatrixOne mm1;
     MatrixOne mm2;
@@ -31,9 +36,9 @@ int main() {
     MatrixOne mm4;
 
 
-    createRamMatrixOne(&mm1, sizelarge, sizelarge, 2);
-
-    //createRamMatrixOne(&mm2, sizelarge, sizelarge, 2);
+    createRamMatrixOne(&mm1, size, size, 2);
+    sleep(1);
+    createRamMatrixOne(&mm2, size, size, 2);
 //    showMatrixOne(&mm1);
 //    showMatrixOne(&mm2);
 
@@ -41,29 +46,22 @@ int main() {
 // showMatrix(&m2);
 
 
-   start0=clock();
-   m3=matmul_plain(&m1, &m2);
-   end0=clock();
-   printf("plain spend time: %lf \n", (double) (end0 - start0) / CLOCKS_PER_SEC);
-//  showMatrix(&m3);
+// m3=matmul_plain(&m1, &m2);//预热
+
+//    start0=clock();
+//    m3=matmul_plain(&m1, &m2);
+//    end0=clock();
+//    printf("plain spend time: %lf \n", (double) (end0 - start0) / CLOCKS_PER_SEC);
+// //  showMatrix(&m3);
 
 
-   start1=clock();
-   m4=matmul_improved(&m1, &m2);
-   end1=clock();
-   printf("improve spend time: %lf\n", (double) (end1 - start1) / CLOCKS_PER_SEC);
+//    start1=clock();
+//    m4=matmul_improved(&m1, &m2);
+//    end1=clock();
+//    printf("improve spend time: %lf\n", (double) (end1 - start1) / CLOCKS_PER_SEC);
 //  showMatrix(&m4);
 
-   start2=clock();
-   m5=matmul_improved11(&m1, &m2);
-   end2=clock();
-   printf("improve11 spend time: %lf\n", (double) (end2 - start2) / CLOCKS_PER_SEC);
-
-    start3 = clock();
-    mm3 = matmul_plainOneD(&mm1, &mm1);
-    end3 = clock();
-    printf("plainOneD spend time: %lf\n", (double) (end3 - start3) / CLOCKS_PER_SEC);
-  //  showMatrixOne(&mm3);
+    // mm4 = matmul_improvedOneD(&mm1, &mm1);//预热
 
 
     start4 = clock();
@@ -74,6 +72,23 @@ int main() {
 
 
 
+    MatrixOne mm5;
 
+    start5 = clock();
+    mm5.row = size;
+    mm5.column = size;
+    mm5.data = (float *) malloc(sizeof(float) * size * size);
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,size,size,size,1,mm1.data, size, mm2.data, size,0,mm5.data,size);
+    end5 = clock();
+    printf("Openblas spend time: %lf\n", (double) (end5 - start5) / CLOCKS_PER_SEC);
+
+//  showMatrixOne(&mm5);
+
+
+    start3 = clock();
+    mm3 = matmul_plainOneD(&mm1, &mm1);
+    end3 = clock();
+    printf("plainOneD spend time: %lf\n", (double) (end3 - start3) / CLOCKS_PER_SEC);
+  //  showMatrixOne(&mm3);,
 
 }
--- source.c
+++ source.c
@@ -91,40 +91,20 @@ MatrixOne matmul_improvedOneD(const MatrixOne *matrix1, const MatrixOne *matrix2
         ans.column = matrix2->column;
 
 
-        float *p1 = (float *) malloc(sizeof(float) * matrix1->column * matrix1->column);
-        if (p1 == NULL) {//申请空间失败
-            printf("p1 is failed to allocated\n");
-        }
+        // float *p1 = (float *) malloc(sizeof(float) * matrix1->column * matrix1->column);
+        // if (p1 == NULL) {//申请空间失败
+        //     printf("p1 is failed to allocated\n");
+        // }
         float *p2 = (float *) malloc(sizeof(float) * matrix2->column * matrix1->column);
         if (p2 == NULL) {//申请空间失败
             printf("p2 is failed to allocated\n");
         }
 
-        int o = 0;
-        int m = 0;
+        size_t o = 0;
+      
 
-// #pragma omp parallel
-        for (int i = 0; i < matrix1->row; ++i) {
-// #pragma omp parallel
 
-            for (int k = 0; k < matrix1->column / 8 * 8; k += 8) {
-                p1[m++] = matrix1->data[i * matrix1->row + k];
-                p1[m++] = matrix1->data[i * matrix1->row + k + 1];
-                p1[m++] = matrix1->data[i * matrix1->row + k + 2];
-                p1[m++] = matrix1->data[i * matrix1->row + k + 3];
-                p1[m++] = matrix1->data[i * matrix1->row + k + 4];
-                p1[m++] = matrix1->data[i * matrix1->row + k + 5];
-                p1[m++] = matrix1->data[i * matrix1->row + k + 6];
-                p1[m++] = matrix1->data[i * matrix1->row + k + 7];
-            }
-            for (int k = matrix1->column / 8 * 8; k < matrix1->column; k++) {
-                p1[m++] = matrix1->data[i * matrix1->row + k];
-            }
-        }
-
-// #pragma omp parallel
         for (int j = 0; j < matrix2->column; ++j) {
-// #pragma omp parallel
             for (int k = 0; k < matrix1->column / 8 * 8; k += 8) {
                 p2[o++] = matrix1->data[k * matrix1->column + j];
                 p2[o++] = matrix1->data[(1 + k) * matrix1->column + j];
@@ -141,13 +121,9 @@ MatrixOne matmul_improvedOneD(const MatrixOne *matrix1, const MatrixOne *matrix2
             }
         }
 
-// int *a;
-// FILE *fp1=fopen("mat64k*6k.txt","w");
-// fscanf(fp1,"%f",&a);
 
 // #pragma omp parallel
         for (int i = 0; i < matrix1->row; ++i) {
-// #pragma omp parallel
             for (int j = 0; j < matrix2->column; ++j) {
                 float temp = 0;
                 float sum[8] = {0};
@@ -157,13 +133,8 @@ MatrixOne matmul_improvedOneD(const MatrixOne *matrix1, const MatrixOne *matrix2
                 int i1 = i * matrix1->column;
                 int j1 = j * matrix1->column;
 
-// #pragma omp parallel
                 for (int k = 0; k < matrix1->column / 8 * 8; k += 8) {
-                    //Use _mm256_mul_ps and _mm256_add_ps to process 8 elements at a time.
-
-                    // matrix1->data[i][k]
-                    //  matrix2->data[k][j]
-                    a = _mm256_loadu_ps(p1 + i1 + k);
+                    a = _mm256_loadu_ps(&matrix1->data[0] + i1 + k);
                     b = _mm256_loadu_ps(p2 + j1 + k);
                     c = _mm256_add_ps(c, _mm256_mul_ps(a, b));
 
@@ -179,6 +150,7 @@ MatrixOne matmul_improvedOneD(const MatrixOne *matrix1, const MatrixOne *matrix2
                 ans.data[i * matrix1->row + j] += temp;
                 temp = 0;
             }
+            // printf("%d\n",i);
         }
     }
 
@@ -206,116 +178,22 @@ Matrix matmul_improved(const Matrix *matrix1, const Matrix *matrix2) {
         initialMatrix(&ans, matrix1->row, matrix2->column);
 
         float *p1 = (float *) malloc(sizeof(float) * matrix1->column * matrix1->column);
+                if (p1 == NULL) {//申请空间失败
+            printf("p1 is failed to allocated\n");
+        }
+
         float *p2 = (float *) malloc(sizeof(float) * matrix2->column * matrix1->column);
-
-        int o = 0;
-        int m = 0;
-
-// #pragma omp parallel
-        for (int i = 0; i < matrix1->row; ++i) {
-// #pragma omp parallel
-
-            for (int k = 0; k < matrix1->column / 8 * 8; k += 8) {
-                p1[m++] = matrix1->data[i][k];
-                p1[m++] = matrix1->data[i][k + 1];
-                p1[m++] = matrix1->data[i][k + 2];
-                p1[m++] = matrix1->data[i][k + 3];
-                p1[m++] = matrix1->data[i][k + 4];
-                p1[m++] = matrix1->data[i][k + 5];
-                p1[m++] = matrix1->data[i][k + 6];
-                p1[m++] = matrix1->data[i][k + 7];
-            }
-            for (int k = matrix1->column / 8 * 8; k < matrix1->column; k++) {
-                p1[m++] = matrix1->data[i][k];
-            }
-        }
-
-// #pragma omp parallel
-        for (int j = 0; j < matrix2->column; ++j) {
-// #pragma omp parallel
-            for (int k = 0; k < matrix1->column / 8 * 8; k += 8) {
-                p2[o++] = matrix1->data[k][j];
-                p2[o++] = matrix1->data[k + 1][j];
-                p2[o++] = matrix1->data[k + 2][j];
-                p2[o++] = matrix1->data[k + 3][j];
-                p2[o++] = matrix1->data[k + 4][j];
-                p2[o++] = matrix1->data[k + 5][j];
-                p2[o++] = matrix1->data[k + 6][j];
-                p2[o++] = matrix1->data[k + 7][j];
-            }
-            for (int k = matrix1->column / 8 * 8; k < matrix1->column; k++) {
-                p2[o++] = matrix1->data[k][j];
-
-            }
+                if (p2 == NULL) {//申请空间失败
+            printf("p2 is failed to allocated\n");
         }
 
 
+        size_t o = 0;
+        size_t m = 0;
+
 // #pragma omp parallel
         for (int i = 0; i < matrix1->row; ++i) {
-// #pragma omp parallel
-            for (int j = 0; j < matrix2->column; ++j) {
-                float temp = 0;
-                float sum[8] = {0};
-                __m256 c = _mm256_setzero_ps();
-                ans.data[i][j] = 0;
 
-                int i1 = i * matrix1->column;
-                int j1 = j * matrix1->column;
-
-// #pragma omp parallel
-                for (int k = 0; k < matrix1->column / 8 * 8; k += 8) {
-                    //Use _mm256_mul_ps and _mm256_add_ps to process 8 elements at a time.
-
-                    // matrix1->data[i][k]
-                    //  matrix2->data[k][j]
-                    a = _mm256_loadu_ps(p1 + i1 + k);
-                    b = _mm256_loadu_ps(p2 + j1 + k);
-                    c = _mm256_add_ps(c, _mm256_mul_ps(a, b));
-
-                }
-                _mm256_storeu_ps(sum, c);
-
-                ans.data[i][j] = sum[0] + sum[1] + sum[2] + sum[3] + sum[4] + sum[5] + sum[6] + sum[7];
-
-                for (int k = matrix1->column / 8 * 8; k < matrix1->column; k += 1) {
-                    //Tail case
-                    temp += matrix1->data[i][k] * matrix2->data[k][j];
-                }
-                ans.data[i][j] += temp;
-                temp = 0;
-            }
-        }
-    }
-    return ans;
-
-
-#else
-    printf("AVX2 is not supported");
-#endif
-}
-
-Matrix matmul_improved11(const Matrix *matrix1, const Matrix *matrix2) {
-#ifdef WITH_AVX2
-    __m256 a, b;
-    __m256 c = _mm256_setzero_ps();
-    Matrix ans;
-    if (matrix1->data == NULL || matrix2->data == NULL) {//判断合法性
-        printf("The matrixs are not valid! \n");
-        ans.data = NULL;
-    } else if (matrix1->column != matrix2->row) {
-        printf("The two matrixs doesn't match! \n");
-        ans.data = NULL;
-    } else {
-
-        initialMatrix(&ans, matrix1->row, matrix2->column);
-
-        float *p1 = (float *) malloc(sizeof(float) * matrix1->column * matrix1->column);
-        float *p2 = (float *) malloc(sizeof(float) * matrix2->column * matrix1->column);
-
-        int o = 0;
-        int m = 0;
-
-        for (int i = 0; i < matrix1->row; ++i) {
             for (int k = 0; k < matrix1->column / 8 * 8; k += 8) {
                 p1[m++] = matrix1->data[i][k];
                 p1[m++] = matrix1->data[i][k + 1];
@@ -358,9 +236,9 @@ Matrix matmul_improved11(const Matrix *matrix1, const Matrix *matrix2) {
 
                 int i1 = i * matrix1->column;
                 int j1 = j * matrix1->column;
+
                 for (int k = 0; k < matrix1->column / 8 * 8; k += 8) {
                     //Use _mm256_mul_ps and _mm256_add_ps to process 8 elements at a time.
-
 
                     // matrix1->data[i][k]
                     //  matrix2->data[k][j]
@@ -389,6 +267,7 @@ Matrix matmul_improved11(const Matrix *matrix1, const Matrix *matrix2) {
     printf("AVX2 is not supported");
 #endif
 }
+
 
 
 void initialMatrix(Matrix *matrix, const int row, const int col) {//初始化一个元素都为0的矩阵
@@ -407,30 +286,29 @@ void initialMatrix(Matrix *matrix, const int row, const int col) {//初始化一
             if (matrix->data[i] == NULL) {
                 printf("The memory is failed to allocated\n");
             }
-            for (int j = 0; j < col; j++) {
-                matrix->data[i][j] = 0;
-            }
+            // for (int j = 0; j < col; j++) {
+            //     matrix->data[i][j] = 0;
+            // }
         }
     }
 }
 
 
-void initialMatrixOne(MatrixOne *matrix, const int row, const int col) {//初始化一个元素都为0的矩阵,一维
-    if (row <= 0 || col <= 0) {
-        printf("invalid matrix, please put in again! \n");
-
-    } else {
-        matrix->row = row;
-        matrix->column = col;
-        matrix->data = (float *) malloc(sizeof(float) * row * col);//申请行空间
-        if (matrix->data == NULL) {//申请空间失败
-            printf("The memory is failed to allocated\n");
-        }
-        // for (size_t j = 0; j < (unsigned long long) row * col; j++) {
-        //     matrix->data[j] = 0;
-        // }
-    }
-}
+// void initialMatrixOne(MatrixOne *matrix, const int row, const int col) {//初始化一个元素都为0的矩阵,一维
+//     if (row <= 0 || col <= 0) {
+//         printf("invalid matrix, please put in again! \n");
+//     } else {
+//         matrix->row = row;
+//         matrix->column = col;
+//         matrix->data = (float *) malloc(sizeof(float) * row * col);//申请行空间
+//         if (matrix->data == NULL) {//申请空间失败
+//             printf("The memory is failed to allocated\n");
+//         }
+//         // for (size_t j = 0; j < (unsigned long long) row * col; j++) {
+//         //     matrix->data[j] = 0;
+//         // }
+//     }
+// }
 
 void createRamMatrix(Matrix *matrix, const int row, const int col, const int databound) {//检查合法性
     srand((unsigned) time(NULL));
@@ -450,7 +328,7 @@ void createRamMatrix(Matrix *matrix, const int row, const int col, const int dat
             }
 
             for (int j = 0; j < col; j++) {
-                float ram = rand() % databound + 1;
+            float ram = (float)rand() / RAND_MAX * 2;
                 matrix->data[i][j] = ram;
             }
         }
@@ -463,15 +341,15 @@ void createRamMatrixOne(MatrixOne *matrix, const int row, const int col, const i
     if (row <= 0) {
         printf("invalid matrix, please put in again! \n");
     } else {
-        // matrix->row = row;
-        // matrix->column = col;
-        // matrix->data = (float *) malloc(sizeof(float) * row * col);//申请行空间
-        initialMatrixOne(matrix, row, col);
+        matrix->row = row;
+        matrix->column = col;
+        matrix->data = (float *) malloc(sizeof(float) * row * col);//申请行空间
+        // initialMatrixOne(matrix, row, col);
         if (matrix->data == NULL) {//申请空间失败
             printf("The memory is failed to allocated\n");
         }
         for (size_t j = 0; j < (unsigned long long) row * col; j++) {
-            float ram = rand() % databound + 1;
+            float ram = (float)rand() / RAND_MAX * 2;
             matrix->data[j] = ram;
         }
 
@@ -499,7 +377,7 @@ void showMatrixOne(const MatrixOne *matrix) {//打印矩阵
     } else {
         for (int i = 0; i < matrix->row; i++) {
             for (int j = 0; j < matrix->column; j++) {
-                printf("%-6g", matrix->data[i * matrix->row + j]);
+                printf("%-10g", matrix->data[i * matrix->row + j]);
             }
             printf("\n");
         }
